ef8224de843b45babed130e55a6b3666679aee7d
urjasrivastava <71330176+urjasrivastava@users.noreply.github.com>
2021-03-15 00:48:54 +0530

    Add files via upload
    Added human detection module

---
+++ main.py
@@ -0,0 +1,24 @@
+import cv2
+import yolo
+capture= cv2.VideoCapture(0)
+frame_width=capture.get(cv2.CAP_PROP_FRAME_WIDTH)
+frame_height=capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+fps=capture.get(cv2.CAP_PROP_FPS)
+
+print("CV_CAP_PROP_FRAME_WIDTH: '{}'".format(frame_width))
+print("CV_CAP_PROP_FRAME_HEIGHT : '{}'".format(frame_height))
+print("CAP_PROP_FPS : '{}'".format(fps))
+
+if capture.isOpened() is False:
+    print("Error opening camera")
+
+ret,frame=capture.read()
+if ret is True:
+    detected=yolo.humanDetect(frame)
+    if detected is not None:
+        cv2.imshow("human detected",detected)
+        cv2.waitKey()
+    else:
+        print('Human Not detected')
+capture.release()
+cv2.destroyAllWindows()
