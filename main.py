3fc89b02293c69076cc879945400392ce6ec8132
Apurva <sambhaviapurva0205@gmail.com>
2021-03-20 19:43:09 +0530

    adding all files

--- main.py
+++ main.py
@@ -1,31 +1,26 @@
 import cv2
-import yolo
-import face_detection
-
-capture= cv2.VideoCapture(0)
-frame_width=capture.get(cv2.CAP_PROP_FRAME_WIDTH)
-frame_height=capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
-fps=capture.get(cv2.CAP_PROP_FPS)
-
-print("CV_CAP_PROP_FRAME_WIDTH: '{}'".format(frame_width))
-print("CV_CAP_PROP_FRAME_HEIGHT : '{}'".format(frame_height))
-print("CAP_PROP_FPS : '{}'".format(fps))
+import YOLO
+import Detect
+capture = cv2.VideoCapture(0)
 
 if capture.isOpened() is False:
-    print("Error opening camera")
-
+	print("Error opening camera")
 ret,frame=capture.read()
 if ret is True:
-    detected=yolo.humanDetect(frame)
-    if detected is not None:
-        #cv2.imshow("human detected",detected)
-        face_detected = face_detection.faceDetect(detected)
-        if face_detected is not None:
-            cv2.imshow("face detected",face_detected)
-        else:
-            print('face not detected')
-    else:
-        print('Human Not detected')
+	detected=YOLO.humanDetect(frame)
+
+	if detected is not None:
+    	
+		#cv2.imshow("human detected",detected)
+		#cv2.waitKey()
+		print("Human Detected")
+		face = Detect.detect_face(detected)
+		cv2.imshow("face", face)
+		mask = Detect.detect_mask(face)
+		print(mask)
+
+	else:
+		print('Human Not detected')
 
 capture.release()
 cv2.waitKey(0)
